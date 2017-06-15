f2a5455796843689c2e6f062a0543a0934d27428
decent <danielekwuazi@gmail.com>
2017-06-16 00:37:32 +0700

    switched to mount from kedira:react-layout

---
+++ client/components/layouts/HomeLayoutComponent.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class HomeLayoutComponent extends Component {
+    render(){
+        return (
+            <div>
+                Header
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+
+                        </div>
+                        <div className="col-md-5 col-md-offset-1">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+console.log("test");
+
+export default HomeLayoutComponent;

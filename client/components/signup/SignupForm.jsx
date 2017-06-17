17933fea674a35b9b7699cd8a3e0ebf46f151588
decent <danielekwuazi@gmail.com>
2017-06-17 23:35:03 +0700

    sign up form in progress

--- client/components/signup/SignupForm.jsx
+++ client/components/signup/SignupForm.jsx
@@ -4,7 +4,18 @@ import react, {Component} from 'react';
      render(){
          return (
              <div className="row">
-                 
+                 <div className="signup">
+                     <h1>Sign Up
+                         <p className="text-muted">Its's free and always will be.</p>
+                     </h1>
+                 </div>
+                 <form action="">
+                     <div className="col-sm-9">
+                         <div className="row"></div>
+                         <div className="form-group"></div>
+                         <div className="form-group"></div>
+                     </div>
+                 </form>
              </div>
          )
      }

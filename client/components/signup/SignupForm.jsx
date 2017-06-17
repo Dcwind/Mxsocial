6d58cd151a95917373ac051d2385a2ebeda466e2
decent <danielekwuazi@gmail.com>
2017-06-17 22:21:59 +0700

    signup form in progress

---
+++ client/components/signup/SignupForm.jsx
@@ -0,0 +1,11 @@
+import react, {Component} from 'react';
+
+ class SignupForm extends Component{
+     render(){
+         return (
+             <div className="row">
+                 
+             </div>
+         )
+     }
+ }

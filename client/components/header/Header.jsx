2550f33bb55b10c97383b03638dc56943b542d9b
decent <danielekwuazi@gmail.com>
2017-06-18 09:21:51 +0700

    in progress

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -7,6 +7,16 @@ export default class Header extends Component{
                 <span className="navbar-react">
                     <i className="fa fa-facebook">Fakebook</i>
                 </span>
+                <div className="collapse navbar-collapse" id="navbar">
+                    <form role="form" id="signin" className="navbar-form navbar-right">
+                        <div className="input-group">
+                            <span className="input-group-addon">
+                                <i className="fa fa-user"></i>
+                            </span>
+                            <input type="text" placeholder="Email or Phone Number" id="email" ref="email" className="form-control"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }

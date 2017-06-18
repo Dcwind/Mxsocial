52074dfcc7a79f608ab271a38a5bce87536e15e0
decent <danielekwuazi@gmail.com>
2017-06-18 09:09:41 +0700

    header component in progress

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 
 export default class Header extends Component{
     render(){
-        
+        return(
+            <div>
+                <span className="navbar-react">
+                    <i className="fa fa-facebook">Fakebook</i>
+                </span>
+            </div>
+        )
     }
 }

4c97c231fe957b86b4cdf02a5911305d419448f5
decent <danielekwuazi@gmail.com>
2017-06-19 12:14:39 +0700

    adding features object

--- client/components/features/FeaturesList.jsx
+++ client/components/features/FeaturesList.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'; 
 
 export default class FeaturesList extends Component{
+    features: [ 
+        {icon:"fa fa-image fa-2x",bigtext:"See photos and updates",littletext:"from friends in News Feed"},
+        {icon:"fa fa-share fa-2x", bigtext:"See photos and updates", littletext:"from friends in News Feed"}
+        {icon:"fa fa-Search fa-2x",bigtext:"See photos and updates",littletext:"from friends in News Feed"} 
+    ]
     render(){
         return(
-            
+
         )
     }
 }

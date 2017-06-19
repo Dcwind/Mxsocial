0441aa899ec92692e415b309a421e054cbbcf4f5
decent <danielekwuazi@gmail.com>
2017-06-19 19:50:36 +0700

    mapping through features list

--- client/components/features/FeaturesList.jsx
+++ client/components/features/FeaturesList.jsx
@@ -3,10 +3,23 @@ import React, {Component} from 'react';
 export default class FeaturesList extends Component{
     features: [ 
         {icon:"fa fa-image fa-2x",bigtext:"See photos and updates",littletext:"from friends in News Feed"},
-        {icon:"fa fa-share fa-2x", bigtext:"See photos and updates", littletext:"from friends in News Feed"}
-        {icon:"fa fa-Search fa-2x",bigtext:"See photos and updates",littletext:"from friends in News Feed"} 
+        {icon:"fa fa-share fa-2x", bigtext:"share lastest happenings", littletext:"with friends on your Timeline"},
+        {icon:"fa fa-Search fa-2x",bigtext:"Find more",littletext:"find of what you\'re looking for with Fakebook search"} 
     ]
     render(){
+        var rows = this.features.map(function(feature){
+            return(
+                <li key={feature.icon}>
+                    <h3 className="btn btn-lg">
+                        <i className={feature.icon}></i>
+                        <span>
+                            <strong>{feature.bigtext}</strong>
+                            <small>{feature.littletext}</small>
+                        </span>
+                    </h3>
+                </li>
+            )
+        })
         return(
 
         )

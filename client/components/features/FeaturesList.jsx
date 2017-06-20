897d0e4be87be1885c9509371757c3767fc89ea3
decent <danielekwuazi@gmail.com>
2017-06-20 23:33:39 +0700

    getFeatures functions

--- client/components/features/FeaturesList.jsx
+++ client/components/features/FeaturesList.jsx
@@ -1,27 +1,43 @@
 import React, {Component} from 'react'; 
 
 export default class FeaturesList extends Component{
-    features: [ 
+    
+
+    getFeatures(){
+    const features = [ 
         {icon:"fa fa-image fa-2x",bigtext:"See photos and updates",littletext:"from friends in News Feed"},
         {icon:"fa fa-share fa-2x", bigtext:"share lastest happenings", littletext:"with friends on your Timeline"},
         {icon:"fa fa-Search fa-2x",bigtext:"Find more",littletext:"find of what you\'re looking for with Fakebook search"} 
     ]
-    render(){
-        var rows = this.features.map(function(feature){
+
+        return features.map(function(feature){
             return(
                 <li key={feature.icon}>
                     <h3 className="btn btn-lg">
                         <i className={feature.icon}></i>
                         <span>
                             <strong>{feature.bigtext}</strong>
-                            <small>{feature.littletext}</small>
+                            <small> {feature.littletext}</small>
                         </span>
                     </h3>
                 </li>
             )
         })
+    }
+    
+    render(){
+        console.log(this.features);
+        
         return(
-
+            <div>
+                <h2 className="col-md-11 featurelist hidden-xs">
+                    Connect with friends and the world around you.
+                </h2>
+                <ul className="ds-btn hidden-xs">
+                    {this.getFeatures()}
+                </ul>
+            </div>
         )
     }
 }
+

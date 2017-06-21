d9e144558a8b7dd0d80327f9a4cc95bce466094d
decent <danielekwuazi@gmail.com>
2017-06-21 18:36:10 +0700

    Navbar imported to dashboard

---
+++ client/components/layouts/DashBoardLayoutComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navbar from '../navbar/Navbar';
+
+class DashBoardLayoutComponent extends Component{
+    render(){
+        return (
+            <div className="wrapper">
+                <div className="box">
+                    <div className="srow row-offcanvas rowoffcanvas-left push-down-50">
+                        <Navbar/>
+                        {this.props.sidebar}
+                        {this.props.content}
+                    </div>
+                </div>   
+            </div>
+        )
+    }
+}
+
+export default DashBoardLayoutComponent;

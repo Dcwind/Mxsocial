f58da6d4326fd5b2b0276c2d2b5f1a55742183c2
decent <danielekwuazi@gmail.com>
2017-06-22 01:35:52 +0700

    Sidebar in progress

---
+++ client/components/sidebar/SideBar.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class SideBar extends Component{
+    // constructor(props){
+    //     super(props);
+        
+    //     this.setState({
+
+    //     });
+    //     this.lalla = this.lalla.bind(this);
+    // }
+
+    render(){
+        return (
+            
+        )
+    }
+}

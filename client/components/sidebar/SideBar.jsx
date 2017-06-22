61f5298dbefa41b4372b07e259c0f7eb23dd830f
decent <danielekwuazi@gmail.com>
2017-06-22 17:39:06 +0700

    SideBar component done

--- client/components/sidebar/SideBar.jsx
+++ client/components/sidebar/SideBar.jsx
@@ -9,25 +9,25 @@ export default class SideBar extends Component{
     //     });
     //     this.lalla = this.lalla.bind(this);
     // }
-    getLink(){
+    getLinks(){
         const links = [
             {_id:1, href:'/profile',icon:'fa fa-user fa-2x',text:'Profile'},
-            {_id:1, href:'/dashboard',icon:'fa fa-rss fa-2x',text:'News Feed'},
-            {_id:1, href:'/messages',icon:'fa fa-comment fa-2x',text:'Messages'},
-            {_id:1, href:'/friends',icon:'fa fa-users fa-2x',text:'Friends'}
+            {_id:2, href:'/dashboard',icon:'fa fa-rss fa-2x',text:'News Feed'},
+            {_id:3, href:'/messages',icon:'fa fa-comment fa-2x',text:'Messages'},
+            {_id:4, href:'/friends',icon:'fa fa-users fa-2x',text:'Friends'}
         ]
 
-        return (
-            link.map((link)=>{
-                <li key={link._id}>
-                    <a href={link.href}>
-                        <i className={link.icon}></i>
-                        {link.text}
-                    </a>
-                </li>
-            })
-        )
-    }
+     return links.map((link)=>{
+                return (
+                    <li key={link._id}>
+                        <a href={link.href}>
+                            <i className={link.icon}></i> {link.text}
+                        </a>
+                    </li>
+                )
+            });
+     
+        }
 
     render(){
         return (
@@ -40,7 +40,7 @@ export default class SideBar extends Component{
                     </li>
                 </ul>
                 <ul className="nav hidden-xs" id="lg-menu">
-
+                    {this.getLinks()}
                 </ul>
             </div>
         )

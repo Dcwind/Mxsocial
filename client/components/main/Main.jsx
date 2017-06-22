6a8e168949c9d8e7796b355771adb1d2b7c8f0ed
decent <danielekwuazi@gmail.com>
2017-06-22 19:13:14 +0700

    Ads section added

--- client/components/main/Main.jsx
+++ client/components/main/Main.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import StatusBar from './StatusBar';
 
 export default class Main extends Component{
     render(){
@@ -6,10 +7,13 @@ export default class Main extends Component{
             <div className="col-sm-9 col-sm-11" id="main">
                 <div className="full col-sm-9">
                     <div className="row">
-                        <div className="col sm 9">
-                            Status Form 
-                            Posts
+                        <div className="col sm 9"> 
+                            <StatusBar/>
+                            Posts 
                             <button className="btn btn-md">More</button>
+                        </div>
+                        <div className="col-sm-3">
+                            Ads
                         </div>
                     </div>
                 </div>

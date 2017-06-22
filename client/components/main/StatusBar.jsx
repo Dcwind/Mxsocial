34dc657c4a29900600340918422317fc78cb686f
decent <danielekwuazi@gmail.com>
2017-06-22 18:37:14 +0700

    image upload functionality in progress

--- client/components/main/StatusBar.jsx
+++ client/components/main/StatusBar.jsx
@@ -1,7 +1,42 @@
 import React, {Component} from 'react';
 
 export default class StatusBar extends Component{
+    constructor(props){
+        super(props)
+
+        this.setState({
+            image:'',
+            filename:''
+        })
+    }
     render(){
-        
+        return(
+            <div className="panel panel-default">
+                <div className="panel-content">
+                    <div className="panel-heading">
+                        update status
+                    </div>
+
+                    <form action="" className="form center-block">
+                        <input type="hidden"  ref="imageid" value={this.state.image}/>
+                        <div className="panel-body">
+                            <div className="form-group">
+                                <textarea id="sharing"  className="form-control input-lg" placeholder="what do you want to share?" ref="sharing">
+
+                                </textarea>
+                                <h3>{this.state.filename||''}</h3>
+                            </div>
+                            <div>
+                                <ul className="pull-left list-inline">
+                                    <li>
+                                        <input type="file" id="file" className="filepicker"/>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
     }
 }

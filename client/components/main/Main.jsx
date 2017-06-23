d999446729e397157ec21adb214a60cff05d562c
decent <danielekwuazi@gmail.com>
2017-06-23 17:34:32 +0700

    ads section

--- client/components/main/Main.jsx
+++ client/components/main/Main.jsx
@@ -5,15 +5,17 @@ export default class Main extends Component{
     render(){
         return(
             <div className="col-sm-9 col-sm-11" id="main">
-                <div className="full col-sm-9">
-                    <div className="row">
-                        <div className="col sm 9"> 
-                            <StatusBar/>
-                            Posts 
-                            <button className="btn btn-md">More</button>
-                        </div>
-                        <div className="col-sm-3">
-                            Ads
+                <div>
+                    <div className="full col-sm-9">
+                        <div className="row">
+                            <div className="col-sm-9"> 
+                                <StatusBar/>
+                                Posts 
+                                <button className="btn btn-md">More</button>
+                            </div>
+                            <div className="col-sm-3"> 
+                                Ads
+                            </div>
                         </div>
                     </div>
                 </div>

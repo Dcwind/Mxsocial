58f9a36c8dc171ac59256715e343b9229f8bf472
decent <danielekwuazi@gmail.com>
2017-06-23 17:42:45 +0700

    in progress

--- client/components/post/Posts.jsx
+++ client/components/post/Posts.jsx
@@ -3,7 +3,21 @@ import React, {Component} from 'react';
 export default class Posts extends Component{
     render(){
         return(
-            <div>post</div>
+          <div className="col-sm-12">
+              <div className="panel panel-white post panel-shadow">
+                  <div className="post-heading">
+                      <div className="pull-left image"> 
+                          Avatar
+                      </div>
+                      <div className="pull-left meta">
+                          <div className="title h5">
+                              <b>Luke Ascarbar</b>
+                              
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
         )
     }
 }

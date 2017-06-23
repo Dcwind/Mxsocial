b0456c29d41f56ed6c544e4c36a860c69452eb38
decent <danielekwuazi@gmail.com>
2017-06-23 19:31:57 +0700

    adObj passed into Ads component

--- client/components/main/Main.jsx
+++ client/components/main/Main.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import StatusBar from './StatusBar';
 import Posts from '../post/Posts';
+import Ads from '../ads/Ads';
 
 export default class Main extends Component{
     render(){
-        var adObj = {_id:1,text:'My First Ad',title:'Some Company',image:'http://placehold.it/200x200'};
+        var adObj = {_id:1,text:'My First Ad',title:'Some Company',image:'http://placehold.it/150x150'};
         return(
             <div className="col-sm-9 col-sm-11" id="main">
                 <div>
@@ -16,7 +17,7 @@ export default class Main extends Component{
                                 <button className="btn btn-md">More</button>
                             </div>
                             <div className="col-sm-3"> 
-                                Ads
+                                <Ads ad={adObj}/>
                             </div>
                         </div>
                     </div>

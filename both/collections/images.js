88b1ed7eeb7da979fcee617a47ffdd631cc3b67b
decent <danielekwuazi@gmail.com>
2017-06-24 23:42:45 +0700

    image stored to filesystem

--- both/collections/images.js
+++ both/collections/images.js
@@ -1,3 +1,15 @@
 Images = new FS.Collection('images',{
-    
+    store:[new FS.Store.FileSystem('images',{path:'~/uploads'})]
+});
+
+Image.allow({
+    insert:()=>{
+        return true;
+    },
+    update:()=>{
+        return true;
+    },
+    remove:()=>{
+        return true;
+    }
 });

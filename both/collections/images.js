0a71a033301501006d5f294f04e9d65f9b42d661
decent <danielekwuazi@gmail.com>
2017-06-25 07:56:31 +0700

    dropping FS.Collection

--- both/collections/images.js
+++ both/collections/images.js
@@ -1,3 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+import {FS} from 'meteor/cfs:filesystem';
+import {Collection} from 'meteor/cfs:collection';
+
 Images = new FS.Collection('images',{
     store:[new FS.Store.FileSystem('images',{path:'~/uploads'})]
 });

373b08a48966d3550ff83612d6a1887c23e2bd38
decent <danielekwuazi@gmail.com>
2017-07-19 10:47:32 +0700

    Avatar component  in use but not yet created

--- imports/ui/components/Profile.jsx
+++ imports/ui/components/Profile.jsx
@@ -9,10 +9,20 @@ export default class Profile extends Component {
                     <div>
                         <label htmlFor="">
                             <div className="inputWrapper">
-                                <input type="file" name="" id="" className="fileInput change-avatar"/>
+                                <input type="file" name="avatar" id="avatar" className="fileInput change-avatar"/>
                             </div>
                         </label>
                     </div>
+                </div>
+                <div className="col-md-9 col-xs-9">
+                    <h2>Luke Ascarbar</h2>
+                    <table className="table table-user-information">
+                        <tbody>
+                            <tr>
+                                <td>Email</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )

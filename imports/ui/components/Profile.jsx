e5c6a320d53dfaeece9b4530f1b4e08451432838
decent <danielekwuazi@gmail.com>
2017-07-19 10:46:41 +0700

    in progress

--- imports/ui/components/Profile.jsx
+++ imports/ui/components/Profile.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 
 export default class Profile extends Component {
     render(){
-        
+        return (
+            <div className="row">
+                <div className="col-md-2 hidden-xs" align="center">
+                Avater
+                    <div>
+                        <label htmlFor="">
+                            <div className="inputWrapper">
+                                <input type="file" name="" id="" className="fileInput change-avatar"/>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }

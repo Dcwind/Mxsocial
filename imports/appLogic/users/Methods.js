c1c94797347b7dbc6d1860b0015333b690af4f54
decent <danielekwuazi@gmail.com>
2017-07-19 22:34:49 +0700

    update email with new email

--- imports/appLogic/users/Methods.js
+++ imports/appLogic/users/Methods.js
@@ -1,7 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
-    'User.changeEmail': ()=> {
-
+    'User.changeEmail': function changeEmail(email) {
+        if (email) {
+            Meteor.users.update(Meteor.userId(), {
+                    $set: {
+                        'emails.0.address': email,
+                    },
+            });
+        }
     },
 })

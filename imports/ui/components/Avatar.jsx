a5cf6a964267801ca0b7615338938ac17ebdcb1f
decent <danielekwuazi@gmail.com>
2017-07-19 13:12:41 +0700

    prop validation

--- imports/ui/components/Avatar.jsx
+++ imports/ui/components/Avatar.jsx
@@ -1,1 +1,16 @@
 import React, { Component, PropTypes } from 'react';
+
+export default class Avatar extends Component {
+    render(){
+
+       
+    }
+}
+
+Avatar.propTypes = {
+    avatar: PropTypes.string,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    klass: PropTypes.string,
+    
+};

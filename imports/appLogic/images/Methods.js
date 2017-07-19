9b1e436ed7f6b9dfcc1785d1e90ce326f641ac25
decent <danielekwuazi@gmail.com>
2017-07-19 11:49:29 +0700

    changeAvatar function

---
+++ imports/appLogic/images/Methods.js
@@ -0,0 +1,19 @@
+
+import { Meteor } from 'meteor/meteor';
+import Images from './images';
+
+Meteor.methods({
+    'Images.changeAvatar': function changeAvatar(imageId) {
+        if (imageId) {
+            const image = Images.findOne({ _id: imageId });
+            if (image !== undefined) {
+                Meteor.users.update(Meteor.userId(), {
+                    $set: {
+                        'profile.avatar': image.link(),
+                    },
+                });
+            }
+        }
+    },
+});
+
